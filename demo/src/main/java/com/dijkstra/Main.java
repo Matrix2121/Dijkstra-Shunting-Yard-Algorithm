ea441910eee80d24c0ff864fea25005f67e51759
Matrix2121 <vasko10.vs38@gmail.com>
2025-04-05 19:10:41 +0300

    Example changed;

--- demo/src/main/java/com/dijkstra/Main.java
+++ demo/src/main/java/com/dijkstra/Main.java
@@ -4,9 +4,6 @@ import com.dijkstra.algorithm.ShuntingYard;
 
 public class Main {
     public static void main(String[] args) {
-        String infix = "a + b ^ c * d / (e + f * g)";
-        ShuntingYard.infixToPostfix(infix);
-        ShuntingYard.infixToPostfix("j + b + c * d / (e + f * g)");
-        ShuntingYard.infixToPostfix("o + b ^ c * d - (e + f * g)");
+        ShuntingYard.infixToPostfix("a + b ^ c * d / (e + f * g)");
     }
 }
